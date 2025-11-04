26aab34285cfea317132af9f4db98f39b0959b84
Nairet Vergara Forsyth <naiforsyth@gmail.com>
2025-11-04 00:10:19 -0300

    Login Page

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,10 @@
+import { loginMethods } from "./pages/login/login.methods";
+
 describe('Login', () => {
-    it('Login con credenciales de standard user', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('#user-name').type('standard_user')
-        cy.get('#password').type('secret_sauce')
-        cy.get('#login-button').click()
-        cy.get('.title').should('exist')
-        
+    it('Login', () => {
+        cy.visit('https://demoblaze.com/')
+        cy.get('a#login2').click()
+        loginMethods.login('username', 'password')
+        cy.wait(5000)
     });
 });
