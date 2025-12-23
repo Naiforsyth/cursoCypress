e0f2984fd0dd338b01aa8ac45f23684b228d04a7
Nairet Vergara Forsyth <naiforsyth@gmail.com>
2025-12-22 22:08:03 -0300

    Creacion de la pagina Place Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testDate(){
+        return{
+            name: 'Test Name',
+            country: 'United State',
+            city: 'New York',
+            creditCardNumber: '4214722996420984',
+            month: 12,
+            year: 2024 
+        }
+    }
+}
